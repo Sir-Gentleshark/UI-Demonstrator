db049ef16e6028d8cf239b380deb578477ab868f
Sir-Gentleshark <the.samn@gmail.com>
2017-09-12 15:38:08 +0400

    First commit

---
+++ Assets/Resources/Scripts/Resize_Script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Resize_Script : MonoBehaviour {
+	public int margin;
+	public int duration;
+	public UIWidget widget;
+	private int width;
+	private int height;
+
+	public void Resize()
+	{
+		height = widget.height;
+		width = widget.width;
+		Debug.Log ("Resize was called");
+		if(height> 0 && width >0)
+		{
+			Debug.Log ("Sprite has size");
+			if (height !=width) 
+			{
+				Debug.Log ("Going Square");
+				width = height;
+			}
+			else 
+			{
+				Debug.Log ("Going Rectangular");
+				width = height + margin;
+			}
+			TweenWidth.Begin (widget,duration,width);
+		}
+	}
+}
